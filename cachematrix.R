6ff2b3c94eb8f2ff25363c24e3a87020f9db5d49
Aishvarya93 <aishvarya.vatsa@hotmail.com>
2016-06-08 17:45:37 +0530

    The program has been changed to get inverse of a given symmetric matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+# The following function returns a special vector ( actually a list), that contains a function
+# to set a value ,get the value, set the mean and get the mean
 
 makeCacheMatrix <- function(x = matrix()) {
-
+m <- NULL
+	set<-function(y) {
+	x<<-y
+	m<<-NULL
+	}
+  		get<-function() x
+        	setmatrix<-function(solve) m <<-solve
+        	getmatrix<-function() m
+      
+	list(set = set, get = get,setmatrix = setmatrix,getmatrix = getmatrix)
 }
 
+# This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+# If the inverse has already been calculated (and the matrix has not changed),
+# then cacheSolve will retrieve the inverse from the cache, denoted by variable 'm'
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve<-function(x=matrix(), ...) {
+        m<-x$getmatrix()
+        			if(!is.null(m)) {
+                		message("getting cached data")
+                		return(m)
+        			}
+        required_matrix<- x$get()
+        m <- solve(required_matrix, ...)
+        x$setmatrix(m)
+        m
 }
